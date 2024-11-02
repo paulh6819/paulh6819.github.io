6c18b0ae5a5ffdeec3c78fbfd477bbc2d05426ca
Paul Henderson <paulh6819@gmail.com>
2024-11-01 22:03:36 -0400

    refactored drop in and image display functionality

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,4 +1,3 @@
-let imagesToBeDisplayedInTheUi = "";
 let formData = new FormData();
 
 async function handleDrop(event) {
@@ -8,39 +7,44 @@ async function handleDrop(event) {
 
   console.log("Dropped file:", file);
 
-  const imageArray = [];
-
-  for (let i = 0; i < file.length; i++) {
-    if (file[i].type.match(/^image\//)) {
-      console.log("Dropped file:", file[i]);
-      formData.append("images[]", file[i]);
-      imageArray.push(file[i]);
-    }
-  }
-
+  // This populates the UI with the uploaded images
+  const imageArray = createArrayOfImagesFromFile(file);
   updateImage(imageArray);
-  function updateImage(imageArray) {
-    //count++;
-
-    for (let image of imageArray) {
-      const img = document.createElement("img");
-
-      img.src = URL.createObjectURL(image);
-      img.height = 10;
-      img.onload = function () {
-        URL.revokeObjectURL(this.src);
-      };
-      imagesToBeDisplayedInTheUi += `<div class="image-dropped-in" >
-<img class="image-dropped-in-url" src="${URL.createObjectURL(
-        image
-      )}" alt="image">
-</div>
-`;
-    }
-    document.getElementById("display-dropped-photos").innerHTML =
-      imagesToBeDisplayedInTheUi;
-  }
 }
+
+/////
+/////  HELPER FUNCTIONS BELOW THIS POINT
+////
+
 function handleDragOver(event) {
   event.preventDefault();
+}
+
+function updateImage(imageArray) {
+  const container = document.getElementById("display-dropped-photos");
+
+  imageArray.forEach((image) => {
+    const imgElement = document.createElement("img");
+    const objectURL = URL.createObjectURL(image);
+    imgElement.src = objectURL;
+
+    imgElement.onload = () => URL.revokeObjectURL(objectURL);
+
+    const div = document.createElement("div");
+    div.className = "image-dropped-in";
+    div.appendChild(imgElement);
+    container.appendChild(div);
+  });
+}
+
+function createArrayOfImagesFromFile(fileElement) {
+  const imageArray = [];
+  for (let i = 0; i < fileElement.length; i++) {
+    if (fileElement[i].type.match(/^image\//)) {
+      console.log("Dropped file:", fileElement[i]);
+      formData.append("images[]", fileElement[i]);
+      imageArray.push(fileElement[i]);
+    }
+  }
+  return imageArray;
 }
